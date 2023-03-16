15ca697a1cdd0f52ebb72d317c91cb571241465a
leogm360 <leogm360@gmail.com>
2023-03-15 23:49:46 -0300

    chore(project): config ssr entry points

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Poppins, Roboto_Mono } from "next/font/google";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const poppins = Poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  fallback: ["swap"],
+});
+
+const robotoMono = Roboto_Mono({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  fallback: ["swap"],
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <div
+      id="fonts-wrapper"
+      className={`${poppins.variable} ${robotoMono.variable}`}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
